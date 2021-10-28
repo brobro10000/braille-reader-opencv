6a2d31e9510b1ee6e61c06375d7cdb1fc66be735
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-10-28 00:25:19 -0400

    express routing

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+// routes
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
